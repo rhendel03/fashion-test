eafea548a8f99b29abe616321cbe9b1856d8b915
rhendel03 <rhendeltorrejos03@gmail.com>
2022-01-31 07:46:52 +0800

    Parse CSV

--- src/sales/sales.controller.ts
+++ src/sales/sales.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Post, Param, Delete, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { parse } from 'papaparse';
+import * as _ from 'lodash';
 
 import { SalesService } from './sales.service';
-import { CreateRecordDto } from './dto/create-record.dto';
 import { diskStorage } from 'multer';
+import { readFileSync } from 'fs';
 
 @Controller('sales')
 export class SalesController {
@@ -12,11 +14,21 @@ export class SalesController {
   @Post('record')
     @UseInterceptors(FileInterceptor('file_asset', { storage : diskStorage({ destination : './files' }) }))
 
-  createRecord(@UploadedFile() file: Express.Multer.File){
-    console.log(file);
-    return file;
+    async createRecord(@UploadedFile() file: Express.Multer.File){
+      const csvFile = readFileSync(file.path);
+      const data = csvFile.toString();
+
+      const parseCsv = await parse(data, {
+        header : true,
+        skipEmptyLines : true,
+        transformHeader: (header) => _.camelCase(header),
+        complete: (results) => results.data
+      });
+      
       //return this.salesService.createRecord(createRecordDto);
-    }
+
+      return parseCsv;
+      }
 
   @Get()
   findAll() {
